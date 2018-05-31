6960514d3b625cae21e09eaca6a19705bce723a0
kello22e <kello22e@mtholyoke.edu>
2018-05-31 00:50:15 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require_relative '../say_hello'
 
 RSpec.configure do |config|
-  # config here
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'default'
 end
